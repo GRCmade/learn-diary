90d4f727f4555ab158dd9507f8289e1ca7901dc5
yuhe <1641284022@qq.com>
2024-08-28 20:06:54 +0800

    8.28

--- uniappx/ts-project-x/src/pages/index/index.vue
+++ uniappx/ts-project-x/src/pages/index/index.vue
@@ -1,72 +1,82 @@
 <template>
-  		<view v-for="(item,index) in list" :key="index">
-			<view @click="click(item)">
-				{{ item }}
-			</view>
+	<view v-for="(item, index) in list" :key="index">
+		<view @click="click({ name: item, index: index } as clickItem)">
+			{{ item + index }}
 		</view>
-		<button @click="GetInfo">GetInfo</button>
+	</view>
+	<button @click="GetInfo">GetInfo</button>
 </template>
 
 <script setup lang="ts">
-import { ref,Ref } from 'vue'
+import { ref, Ref } from 'vue'
+
+type clickItem  = {
+	name: string
+	index: number
+}
+
 type Item = {
-		name : string
-		select : boolean
-	}
-	let item : Item = {
-		name: "yuhe",
-		select: false
-	}
-	const list = ref<Item[]>([])
-	list.value.push(item)
-	list.value.push(item)
-	list.value.push(item)
-	const click = (item : Item) => {
-		item.select = !item.select
-	}
-	const GetInfo = () => {
-		console.log(list.value)
-	}
+	name: string
+	select: boolean
+}
+let item: Item = {
+	name: "yuhe",
+	select: false
+}
+// const list = ref<Item[]>([])
+const list = ref(["yuhe", "yuhe1"])
+// list.value.push(item)
+// list.value.push(item)
+// list.value.push(item)
+const click = (which: any) => {
+	// item.select = !item.select
+	console.log(which)
+}
+const GetInfo = () => {
+	console.log(list.value)
+}
+
+console.log(list?.yuhe)
 </script>
 
 <style>
 .box {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 }
 
 .circle {
-  margin-right: 10px;
-  width: 20px;
-  height: 20px;
-  border-radius: 10px;
-  background-color: red;
+	margin-right: 10px;
+	width: 20px;
+	height: 20px;
+	border-radius: 10px;
+	background-color: red;
 }
 
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 }
 
 .logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
 }
 
 .text-area {
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 
 .title {
-  font-size: 36rpx;
-  color: #8f8f94;
+	font-size: 36rpx;
+	color: #8f8f94;
 }
 </style>
